f9077ec99ccfffdc275fab78e882252800bfc272
suban <nursuluu@gmx.de>
2022-07-08 15:25:42 +0200

    commit2

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class Main {
+    public static void main(String[] args){
+        Triangel triangel = new Triangel();
+        triangel.a = 5;
+        triangel.b = 4;
+        triangel.c = 7;
+
+        triangel.area();
+        
+
+
+    }
+
+}
